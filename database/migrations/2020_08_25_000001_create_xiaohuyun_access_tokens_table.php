5034f633cec2324dced8210e6ef2f9813a0692ad
feng2009 <lyf1918@163.com>
2020-08-26 11:18:42 +0800

    修复已知bug

--- database/migrations/2020_08_25_000001_create_xiaohuyun_access_tokens_table.php
+++ database/migrations/2020_08_25_000001_create_xiaohuyun_access_tokens_table.php
@@ -13,7 +13,7 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('xiaohuyun_personal_access_tokens', function (Blueprint $table) {
+        Schema::create('xiaohuyun_access_tokens', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->morphs('tokenable');
             $table->string('name');
@@ -31,6 +31,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('xiaohuyun_personal_access_tokens');
+        Schema::dropIfExists('xiaohuyun_access_tokens');
     }
 }
